6677f49c3b4811ea79106db5df3a0d34c2ed5014
Digvijay Gunjal <digvijag@INdigvijag.local>
2014-06-01 16:41:28 +0530

    creating a rails project for library

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = 'e73eb50223605811e9064ca4d61c1e75d95cbb1b62b47a20f213876e9a458e7b6d31b7bb62aef7fa69368a94d04131db2d154aec43f7f81ec1b63129c653dfe4'
